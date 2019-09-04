c1c1f85c60c51f20344d1d6bbdb372ef16dd6ff1
Lynn Barlow <lynn.b.barlow3@gmail.com>
2019-09-04 17:07:54 -0600

    added functions script

--- 2D_Platformer/Assets/Scripts/Assignments/Variables.cs
+++ 2D_Platformer/Assets/Scripts/Assignments/Variables.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Variables : MonoBehaviour
 {
-    string name = "Lynnerd";
+    string fName = "Lynnerd";
     int age = 27;
     float height = 6.2f;
     bool mStatus = false;
@@ -14,7 +14,7 @@ public class Variables : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("First Name: " + name);
+        Debug.Log("First Name: " + fName);
         Debug.Log("Age: " + age);
         Debug.Log("Height: " + height);
         Debug.Log("Maritial Status: " + mStatus);
